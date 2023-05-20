79239573202b5be89bbf7a5730282102ee6b1874
Victor Silalahi <victorbiz766hi@gmail.com>
2023-05-20 22:52:21 +0700

    modified:   app/static/js/apps/chat.js 	modified:   app/templates/chat.html

--- app/static/js/apps/chat.js
+++ app/static/js/apps/chat.js
@@ -1,10 +1,12 @@
 
 var token = null;
 var sio;
+var room_name = '';
 
 $(document).ready(function() {
     
     token = localStorage.getItem('jwt-token');
+    var username = localStorage.getItem("username");
 
     if (token) {
         loadRooms(token);
@@ -15,6 +17,38 @@ $(document).ready(function() {
     $("#logoutid").on("click", function() {
         logOut();
     });
+
+    $("#txtMessage").on("keyup", function(e) {
+        if (e.keyCode == 13) {
+
+            if (room_name == '') {
+                alert("Choose a room for conversation!");
+                return false;
+            }
+    
+            if ($(this).val()=='') {
+                alert("Please type your sentence!");
+                $(this).focus();
+            } else {
+                let msg = $(this).val();
+                msg_to_send = msg.trim();
+    
+                sio.emit("send_message", {
+                    "username": username,
+                    "room_name": room_name,
+                    "message": msg_to_send
+                });
+    
+                $(this).val("");
+                $(this).focus();
+    
+            }
+    
+        }
+    });
+
+    $("#txtMessage").focus();
+
 });
 
 function logOut() {
@@ -27,6 +61,8 @@ function loadRooms(token) {
 
     var member_id = localStorage.getItem("member_id");
     var username = localStorage.getItem("username");
+
+    $("#lblUsername").html("<h4><b>"+username+"</b></h4>");
 
     $.ajax({
         url:"http://127.0.0.1:5000/api/member/rooms",
@@ -57,11 +93,23 @@ function loadRooms(token) {
             var dat = data['data'];
             var str = "";
             var rooms = [];
+
+
             for (var i=0; i<dat.length; i++) {
                 str = str + "<li class='list-group-item d-flex justify-content-between align-items-center' id='"+dat[i][0]+"'>";
                 str = str + "<h4><b>"+dat[i][1]+"</b>";
                 str = str + "<span class='badge badge-error'></span></li></h4>";
                 rooms.push(dat[i][1]);
+
+                var new_div = document.createElement("div");
+                new_div.setAttribute("id",dat[i][1]);
+                new_div.style.width = "100%";
+                new_div.style.maxHeight = "600px";
+                new_div.style.overflowY = "auto";
+                new_div.style.display = "none";
+
+                $("#divConversation").append(new_div);
+
             }
             $("#groupList").html(str);
 
@@ -75,12 +123,12 @@ function loadRooms(token) {
 
             sio.on("join_room_announcement", function(data) {
                 console.log(data);
-                $(".room-list").find("li").each(function() {
-                    if ($(this).text()==data["room_name"]) {
-                        $(this).find(".badge").html("<b>!</b>");
-                        
-                    }
-                });
+                findRoomInList(data["room_name"], "join_room", data["username"], "");
+            });
+
+            sio.on("receive_message", function(data) {
+                console.log(data);
+                findRoomInList(data["room_name"], "message", data["username"], data["message"]);
             });
         
         }
@@ -88,88 +136,70 @@ function loadRooms(token) {
 
 
 }
+function findRoomInList(room_name, event_type, username, msg) {
 
-function loadRoomMessage(room_id, token) {
+    $(".room-list").find("li").each(function() {
+        var temp_room_name = $(this).text();
+        var li_room_name;
+        if (temp_room_name.includes("!")==true) {
+            li_room_name = temp_room_name.replace("!","");
+        } else {
+            li_room_name = temp_room_name;
+        }
+        if (li_room_name==room_name) {
+            $(this).find(".badge").html("<b>!</b>");
+
+            var childDiv = document.createElement("div");
+            if (event_type=="join_room") {
+                childDiv.innerHTML = "<i><b>"+username+ "</b> has joined the room.</i>";
+            }
+            else
+            {
+                childDiv.innerHTML = "<b>["+username+ "]</b>&nbsp;"+msg;
+            }
+            $("#"+room_name).append(childDiv);
+            $("#"+room_name).scrollTop(20000);
+        }
+    });
+}
+
+function loadRoomMessage(room, token) {
 
 }
 
 function messageDelete(room_id, conversation_id, token) {
 
-    $.ajax({
-        url:"http://127.0.0.1:5000/api/member/message",
-        data: {room_id: room_id, conversation_id: conversation_id},
-        method:"delete",
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader("Authorization", "Bearer "+token);
-        },
-        statusCode: {
-            404: function(responseObject, textStatus, jqXHR) {
-                // No content found (404)
-                // This code will be executed if the server returns a 404 response
-                alert("Content not found!");
-            },
-            503: function(responseObject, textStatus, errorThrown) {
-                // Service Unavailable (503)
-                // This code will be executed if the server returns a 503 response
-                alert("Service Unavailable!");
-            },
-            401: function(responseObject, textStatus, errorThrown) {
-                // Unauthorized
-                alert("Unauthorized User!");
-                logOut();
-            }           
-        }, 
-        success: function(data) {
-            loadTodos(token);
-        }
-    });        
 
 }
 
 function messageAdd(token) {
 
-    const title = $("#modalAddEditTodo #txtTitle").val();
-    const desc = $("#modalAddEditTodo #txtDesc").val();
-
-    $.ajax({
-        url:"http://127.0.0.1:5000/api/todo",
-        data: {title: title, desc: desc},
-        method:"post",
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader("Authorization", "Bearer "+token);
-        },
-        statusCode: {
-            404: function(responseObject, textStatus, jqXHR) {
-                // No content found (404)
-                // This code will be executed if the server returns a 404 response
-                alert("Content not found!");
-            },
-            503: function(responseObject, textStatus, errorThrown) {
-                // Service Unavailable (503)
-                // This code will be executed if the server returns a 503 response
-                alert("Service Unavailable!");
-            },
-            401: function(responseObject, textStatus, errorThrown) {
-                // Unauthorized
-                alert("Unauthorized User!");
-                logOut();
-            }           
-        }, 
-        success: function(data) {
-            loadTodos(token);
-        }
-    });        
-
 }
 
 $(document).on("click", ".list-group-item", function() {
+
     $(".list-group-item").removeClass("active");
     $(this).addClass("active");
     var c = $(this).find(".badge");
     c.text("");
 
-    // tampilkan percakapan di dalam room yg dipilih
-    
+    room_name = $(this).text();
+
+    var choosen_room = $(this).text();
+
+    $(".room-list li").each(function() {
+        var new_room_name;
+        var room_name = $(this).text();
+        if (room_name.includes("!")) {
+            new_room_name = room_name.replace("!","");
+        } else {
+            new_room_name = room_name; 
+        }
+        $("#"+new_room_name).hide();
+    });    
+
+    $("#spanRoom").html("<b>"+choosen_room+"</b>");
+    $("#"+choosen_room).show();
 });
 
 
